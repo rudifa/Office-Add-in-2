890a960350eda106cc40ed4e3662d8461bd6bee1
Rudi Farkas <rudi.farkas@gmail.com>
2022-11-05 16:38:12 +0100

    reorder and clean up

--- src/taskpane/github-connect.js
+++ src/taskpane/github-connect.js
@@ -1,24 +1,5 @@
-// export class GithubConnect {
-//   constructor() {
-
-//   }
-
-//   githubUsersUrl = 'https://api.github.com/users/'
-//   userDataKeys = ["login", "name", "location", "bio"];
-
-//   async function fetchUserData(userName) {
-//     // fetch the user's data from the GitHub API.
-//     const url = `${githubUsersUrl}${userName}`;
-//     const obj = await fetchFrom(url, `github user ${userName} not found`);
-//     // prepare the data for the table.
-//     const userData = userDataKeys.map((key) => obj[key] || "");
-//     console.log(`fetchUserData`, userData);
-//     return userData;
-//   }
-// }
-
 const githubUsersUrl = "https://api.github.com/users/";
-const userDataKeys = ["login", "name", "location", "bio"];
+export const userDataKeys = ["login", "name", "location", "bio"];
 
 export async function fetchUserData(userName) {
   // fetch the user's data from the GitHub API.
@@ -35,10 +16,9 @@ export async function fetchUserData(userName) {
  * @param {*} url
  * @returns promise that resolves to the JSON object returned by the url
  */
- async function fetchFrom(url, errorMessage) {
+async function fetchFrom(url, errorMessage) {
   const response = await fetch(url);
   if (!response.ok) {
-    //console.log(response);
     throw new Error(`${errorMessage}`);
   }
   const data = await response.json();
--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -6,10 +6,8 @@
 /* global Office, Word */
 
 import { base64Image } from "../../base64Image";
-// import { GithubConnect } from "./github-connect";
 
-import * as ghc from "./github-connect";
-
+import * as GHConnect from "./github-connect";
 
 Office.onReady((info) => {
   if (info.host === Office.HostType.Word) {
@@ -45,152 +43,14 @@ Office.onReady((info) => {
   }
 });
 
-async function insertGithubUsersTable() {
-  await Word.run(async (context) => {
-    const tableData = [["login", "name", "location", "bio"]];
-    const table = context.document.body.insertTable(tableData.length, tableData[0].length, "Start", tableData);
-    table.headerRowCount = 1;
-    table.styleBuiltIn = Word.Style.gridTable5Dark_Accent2;
-    await context.sync();
-  });
-}
+/**
+ * Interactions with the UI
+ */
 
-async function _addRow(context, firstTable, userData) {
-  const tableData = [userData];
-  firstTable.addRows("End", tableData.length, tableData);
-  await context.sync();
-}
-
-async function _fetchUserData() {
-  const userName = getUserName();
-  // fetch the user's data from the GitHub API.
-  const url = `https://api.github.com/users/${userName}`;
-  const obj = await fetchFrom(url, `github user ${userName} not found`);
-  // prepare the data for the table.
-  const userData = ["login", "name", "location", "bio"].map((key) => obj[key] || "");
-  console.log(`updateGithubUserData`, userData);
-  return userData;
-}
-
-// Helper function to get user name from input element
 function getUserName() {
   const userName = document.getElementById("github-user-name").value;
   console.log(`getUserName`, userName);
   return userName;
-}
-
-async function findOrInsertGithubUsersTable() {
-  await Word.run(async (context) => {
-    const tableFound = await findGithubUsersTable();
-    console.log(`findOrInsertGithubUsersTable`, tableFound);
-    if (!tableFound) {
-      await insertGithubUsersTable();
-    } else {
-      displayInfoMessage("Table already exists");
-    }
-  });
-}
-
-async function findGithubUsersTable() {
-  var tableFound = false;
-  await Word.run(async (context) => {
-    // find a table with the tag "githubUsers"
-    const tableCollection = context.document.body.tables;
-    // Queue a commmand to load the results.
-    context.load(tableCollection);
-    await context.sync();
-    //cycle through the table collection and test the first cell of each table looking for insects
-    for (var i = 0; i < tableCollection.items.length; i++) {
-      const table = tableCollection.items[i];
-      var cell00 = table.values[0][0];
-      if (cell00 == "login") {
-        tableFound = true;
-        break;
-      }
-    }
-    console.log(`findGithubUsersTable`, tableFound);
-  });
-  return tableFound;
-}
-
-async function updateGithubUserData() {
-  await Word.run(async (context) => {
-    var tableFound = await _findGithubUsersTable(context);
-    console.log(`updateGithubUserData`, tableFound);
-    if (!tableFound) {
-      await insertGithubUsersTable();
-      tableFound = await _findGithubUsersTable(context);
-    }
-    context.load(tableFound);
-    await context.sync();
-    console.log(`updateGithubUserData loaded tableFound`, tableFound);
-
-    // await _logAllCellValues(context, tableFound);
-
-    const userData = await _fetchUserData();
-    const userName = userData[0];
-    const rowFound = await _findMatchingRow(context, tableFound, userName);
-    console.log(`updateGithubUserData rowFound`, rowFound);
-
-    if (!rowFound) {
-      await _addRow(context, tableFound, userData);
-    } else {
-      await _updateRow(context, rowFound, userData);
-    }
-  });
-}
-
-async function _updateRow(context, rowFound, userData) {
-  console.log(`_updateRow`, rowFound, userData);
-  rowFound.values = [userData];
-  await context.sync();
-}
-
-async function _findMatchingRow(context, tableFound, cell0Value) {
-  const tableRows = tableFound.rows;
-  context.load(tableRows);
-  await context.sync();
-  console.log(`_findMatchingRow loaded tableRows`, tableRows);
-  let rowFound = null;
-  for (var i = 0; i < tableRows.items.length; i++) {
-    const tableRow = tableRows.items[i];
-    console.log(`_findMatchingRow tableRow 1`, tableRow);
-    const tableRowCells = tableRow.cells;
-    context.load(tableRow);
-    context.load(tableRowCells);
-
-    await context.sync();
-    console.log(`_findMatchingRow tableRow 2`, tableRow);
-
-    console.log(`_findMatchingRow loaded tableRowCells ${i}`, tableRowCells);
-    const tableRowCell0 = tableRowCells.items[0];
-    console.log(`_findMatchingRow loaded tableRowCell ${i}`, tableRowCell0.value);
-    if (tableRowCell0.value === cell0Value) {
-      console.log(`_findMatchingRow found matching row`, tableRow);
-      rowFound = tableRow;
-      console.log(`_findMatchingRow rowFound`, rowFound);
-    }
-  }
-  console.log(`_findMatchingRow rowFound`, rowFound);
-  return rowFound;
-}
-
-async function _findGithubUsersTable(context) {
-  const tableCollection = context.document.body.tables;
-  // Queue a commmand to load the results.
-  context.load(tableCollection);
-  await context.sync();
-  var tableFound = null;
-  //cycle through the table collection and test the first cell of each table looking for insects
-  for (var i = 0; i < tableCollection.items.length; i++) {
-    const table = tableCollection.items[i];
-    var cell00 = table.values[0][0];
-    if (cell00 == "login") {
-      tableFound = table;
-      break;
-    }
-  }
-  return tableFound;
 }
 
 function displayAddinInfo() {
@@ -202,52 +62,6 @@ function displayAddinInfo() {
 function clearAddinInfo() {
   displayInfoMessage("");
 }
-
-/**
- * Leftovers from earlier code versions
- */
-
-async function testAsync() {
-  const userData = await ghc.fetchUserData(getUserName());
-  console.log(`testAsync`, userData);
-  displayInfoMessage(`testAsync ${userData}`);
-}
-
-async function _logAllCellValues(context, tableFound) {
-  const tableRows = tableFound.rows;
-  context.load(tableRows);
-  await context.sync();
-  console.log(`_logAllCellValues loaded tableRows`, tableRows);
-  for (var i = 0; i < tableRows.items.length; i++) {
-    const tableRow = tableRows.items[i];
-    const tableRowCells = tableRow.cells;
-    context.load(tableRowCells);
-    await context.sync();
-    console.log(`_logAllCellValues loaded tableRowCells ${i}`, tableRowCells);
-    for (var j = 0; j < tableRowCells.items.length; j++) {
-      const tableRowCell = tableRowCells.items[j];
-      //const tableRowCellValue = tableRowCell.values[0][0];
-      console.log(`_logAllCellValues loaded tableRowCell ${i},${j}`, tableRowCell.value);
-    }
-  }
-}
-
-async function updateGithubUserData_0() {
-  // get the user's name from the user input.
-  const userData = await _fetchUserData();
-  // get the user data from the table (if present)
-
-  // append to the first table in the document
-  // TODO find the table
-  await Word.run(async (context) => {
-    const firstTable = context.document.body.tables.getFirst();
-    await _addRow(context, firstTable, userData);
-  });
-}
-
-/**
- * utilities
- */
 
 function displayInfoMessage(message) {
   displayMessage(message, "blue");
@@ -264,19 +78,107 @@ function displayMessage(message, color) {
 }
 
 /**
- * Fetch data from a URL
- * @param {*} url
- * @returns promise that resolves to the JSON object returned by the url
+ * Interactions with the document
  */
-async function fetchFrom(url, errorMessage) {
-  const response = await fetch(url);
-  if (!response.ok) {
-    //console.log(response);
-    throw new Error(`${errorMessage}`);
-  }
-  const data = await response.json();
-  return data;
+
+async function insertGithubUsersTable() {
+  await Word.run(async (context) => {
+    const tableHeaderData = [GHConnect.userDataKeys];
+    const table = context.document.body.insertTable(
+      tableHeaderData.length,
+      tableHeaderData[0].length,
+      "Start",
+      tableHeaderData
+    );
+    table.headerRowCount = 1;
+    table.styleBuiltIn = Word.Style.gridTable5Dark_Accent2;
+    await context.sync();
+  });
 }
+
+async function findOrInsertGithubUsersTable() {
+  await Word.run(async (context) => {
+    const tableFound = await _findGithubUsersTable(context);
+    if (tableFound) {
+      displayInfoMessage("Table already exists");
+    } else {
+      await insertGithubUsersTable();
+    }
+  });
+}
+
+async function updateGithubUserData() {
+  await Word.run(async (context) => {
+    let tableFound = await _findGithubUsersTable(context);
+    if (!tableFound) {
+      await insertGithubUsersTable();
+      tableFound = await _findGithubUsersTable(context);
+    }
+    context.load(tableFound);
+    await context.sync();
+
+    const userName = getUserName();
+    const userData = await GHConnect.fetchUserData(userName);
+    const rowFound = await _findMatchingRow(context, tableFound, userName);
+
+    if (!rowFound) {
+      await _addRow(context, tableFound, userData);
+    } else {
+      await _updateRow(context, rowFound, userData);
+    }
+  });
+}
+
+async function _addRow(context, table, userData) {
+  const tableData = [userData];
+  table.addRows("End", tableData.length, tableData);
+  await context.sync();
+}
+
+async function _updateRow(context, rowFound, userData) {
+  console.log(`_updateRow`, rowFound, userData);
+  rowFound.values = [userData];
+  await context.sync();
+}
+
+async function _findMatchingRow(context, table, cell0Value) {
+  const tableRows = table.rows;
+  context.load(tableRows);
+  await context.sync();
+  let rowFound = null;
+  for (const tableRow of tableRows.items) {
+    const tableRowCells = tableRow.cells;
+    context.load(tableRow);
+    context.load(tableRowCells);
+    // eslint-disable-next-line office-addins/no-context-sync-in-loop
+    await context.sync();
+
+    const tableRowCell0 = tableRowCells.items[0];
+    if (tableRowCell0.value === cell0Value) {
+      rowFound = tableRow;
+    }
+  }
+  return rowFound;
+}
+
+async function _findGithubUsersTable(context) {
+  const tableCollection = context.document.body.tables;
+  context.load(tableCollection);
+  await context.sync();
+  let tableFound = null;
+  // cycle through the table collection and look for a matching cell[0][0] value
+  for (const table of tableCollection.items) {
+    if (table.values[0][0] == GHConnect.userDataKeys[0]) {
+      tableFound = table;
+      break;
+    }
+  }
+  return tableFound;
+}
+
+/**
+ * utilities
+ */
 
 // Default helper for invoking an action and handling errors.
 async function tryCatch(callback) {
@@ -286,14 +188,17 @@ async function tryCatch(callback) {
   } catch (error) {
     console.log(error);
     displayErrorMessage(error);
-
-    if (error instanceof OfficeExtension.Error) {
-      const debugInfo = JSON.stringify(error.debugInfo);
-      console.log("Debug info: " + debugInfo);
-    }
-    // OfficeHelpers.UI.notify(error);
-    // OfficeHelpers.Utilities.log(error);
   }
+}
+
+/**
+ * Testing functions
+ */
+
+async function testAsync() {
+  const userData = await GHConnect.fetchUserData(getUserName());
+  console.log(`testAsync`, userData);
+  displayInfoMessage(`testAsync ${userData}`);
 }
 
 /**
@@ -420,44 +325,3 @@ async function replaceContentInControl() {
     await context.sync();
   });
 }
-
-/*
-addRows(insertLocation:  "Start" | "End", rowCount: number, values?: string[][]): Word.TableRowCollection;
-*/
-
-/* https://xomino.com/category/office-add-in/
-async function runAllTables() {
-  await Word.run(async (context) => {
-    const tableCollection = context.document.body.tables;
-    // Queue a commmand to load the results.
-    context.load(tableCollection);
-    await context.sync();
-    //cycle through the tbale collection and test the first cell of each table looking for insects
-    for (var i = 0; i < tableCollection.items.length; i++) {
-      var theTable = null;
-      theTable = tableCollection.items[i];
-      var cell1 = theTable.values[0][0];
-      if (cell1 == "Insects") {
-        //once found, load the table in memory and add a row
-        context.load(theTable, "");
-        await context.sync();
-        let numRows = theTable.rowCount.toString();
-        theTable.addRows("End", 1, [[numRows, "Lightning Bug"]]);
-      }
-    }
-  });
-}
-*/
-
-/*
-$("#run").click(() => tryCatch(run));
-
-async function run() {
-    await Word.run(async (context) => {
-        const range = context.document.getSelection();
-        range.font.color = "blue";
-
-        await context.sync();
-    });
-}
-*/
